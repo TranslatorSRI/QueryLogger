a7284be7b33ea0525d003a7cee1533a73e1ff772
Max Wang <max@covar.com>
2023-03-29 14:20:12 -0400

    Add support for different ARS environments

--- server.py
+++ server.py
@@ -17,30 +17,69 @@ APP = FastAPI(
   },
 )
 
+
+@APP.get("/api/ars")
+async def get_ars_urls():
+  """GET list of ARS urls for each environment."""
+  ars_urls = []
+  try:
+    async with httpx.AsyncClient(timeout=10) as client:
+      response = await client.get("https://smart-api.info/api/query?q=4c12efd48ced755ac4b72b1922202ec2")
+      response = response.json()
+      for hit in response["hits"]:
+        for server in hit["servers"]:
+          ars_urls.append(server)
+  except Exception as e:
+    print(e)
+  return ars_urls
+
+
 @APP.get("/api/logs")
 async def get_logs(ars_url: str, ara: str):
   """GET the ARS log of all queries in the last 24 hours for a specific ARA."""
   async with httpx.AsyncClient(timeout=60) as client:
-    url = ars_url + "/" + ara
+    url = f"{ars_url}/ars/api/reports/{ara}"
     response = await client.get(
       url=url,
     )
     return response.json()
 
 
-@APP.get("/api/aras")
-async def get_aras():
-  """Get list of aras by infores."""
-  inforeses = []
+@APP.get("/api/actors")
+async def get_aras(ars_url: str):
+  """
+  Get list of aras by ars actor.
+  
+  Example actor:
+  {
+    "model": "tr_ars.actor",
+    "pk": 1,
+    "fields": {
+      "name": "ara-aragorn-runquery",
+      "channel": [
+        "general",
+        "workflow"
+      ],
+      "agent": "ara-aragorn",
+      "urlRemote": "https://aragorn.renci.org/aragorn/asyncquery",
+      "path": "http://ars-dev.transltr.io/ara-aragorn/api/runquery",
+      "active": true
+    }
+  }
+  """
+  actors = []
   try:
     async with httpx.AsyncClient(timeout=10) as client:
-      response = await client.get('https://smart-api.info/api/query?q=info.x-translator.component:ARA&fields=info.x-translator.infores')
+      response = await client.get(f"{ars_url}/ars/api/actors")
       response = response.json()
-      inforeses = [hit['info']['x-translator']['infores'].split(":")[1] for hit in response['hits'] if 'info' in hit]
+      for actor in response:
+        # loop through all actors and send back all active ones
+        if actor["fields"]["active"]:
+          actors.append(actor["fields"]["agent"].split("-", 1)[1])
   except Exception as e:
     print(e)
   # return only unique inforeses
-  return list(set(inforeses))
+  return list(set(actors))
 
 # servers UI
 APP.mount("/", StaticFiles(directory="ui/build", html=True), name="ui")
--- ui/App.jsx
+++ ui/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-useless-fragment */
 import React, { useState, useEffect, useCallback } from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import { StyledEngineProvider, ThemeProvider, createTheme } from '@mui/material/styles';
@@ -6,6 +7,10 @@ import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import Button from '@mui/material/Button';
 import CircularProgress from '@mui/material/CircularProgress';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 import Logs from './Logs';
 
@@ -15,76 +20,131 @@ import cssTheme from './cssTheme.json';
 const theme = createTheme(cssTheme);
 
 export default function App() {
-  // const [ars_urls, set_ars_urls] = useState([]);
+  const [ars_instances, set_ars_instances] = useState([]);
+  const [selected_ars, set_selected_ars] = useState('');
   const [aras, set_aras] = useState([]);
   const [ara, set_ara] = useState('');
   const [logs, set_logs] = useState(null);
-  const [loading_aras, set_loading_aras] = useState(true);
+  const [loading_ars_urls, set_loading_ars_urls] = useState(true);
+  const [loading_aras, set_loading_aras] = useState(false);
   const [loading_logs, set_loading_logs] = useState(false);
 
   useEffect(() => {
-    fetch('/api/aras')
+    fetch('/api/ars')
+      .then((res) => res.json())
+      .then((data) => {
+        set_ars_instances(data);
+      })
+      .catch((err) => console.error(err))
+      .finally(() => {
+        set_loading_ars_urls(false);
+      });
+  }, []);
+
+  const handleArsSelector = useCallback((e) => {
+    set_selected_ars(e.target.value);
+    set_loading_aras(true);
+    fetch(`/api/actors?${new URLSearchParams({
+      ars_url: e.target.value,
+    })}`)
       .then((res) => res.json())
       .then((data) => {
         set_aras(data);
-        set_loading_aras(false);
       })
-      .catch((err) => console.error(err));
-  }, []);
+      .catch((err) => console.error(err))
+      .finally(() => {
+        set_loading_aras(false);
+      });
+  }, [ars_instances]);
 
   useEffect(() => {
     set_logs(null);
-  }, [ara]);
+  }, [ara, selected_ars]);
 
   const get_logs = useCallback(() => {
     set_loading_logs(true);
     fetch(`/api/logs?${new URLSearchParams({
-      ars_url: 'https://ars-dev.transltr.io/ars/api/reports',
+      ars_url: selected_ars,
       ara,
     })}`)
       .then((res) => res.json())
       .then((data) => {
         set_logs(data);
-        set_loading_logs(false);
       })
-      .catch((err) => console.error(err));
-  }, [ara]);
+      .catch((err) => console.error(err))
+      .finally(() => {
+        set_loading_logs(false);
+      });
+  }, [ara, selected_ars]);
 
   return (
     <StyledEngineProvider injectFirst>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div id="mainContainer">
-          <h1 id="header">DEV ARS Query Logger</h1>
-          {!loading_aras ? (
+          <h1 id="header">ARS Query Logger</h1>
+          {!loading_ars_urls ? (
             <>
-              <Autocomplete
-                className="araSelector"
-                disablePortal
-                freeSolo
-                options={aras}
-                renderInput={(params) => <TextField {...params} label="ARA" />}
-                inputValue={ara}
-                onInputChange={(e, v) => set_ara(v)}
-              />
-              <Button
-                variant="contained"
-                onClick={get_logs}
-                disabled={!ara}
+              <FormControl
+                sx={{
+                  width: '50%',
+                }}
               >
-                Load Logs
-              </Button>
-              {(logs !== null || loading_logs) && (
-                <div id="logsContainer">
-                  <h2>Logs:</h2>
-                  {!loading_logs ? (
-                    <Logs logs={logs} />
+                <InputLabel id="ars_url_selector">ARS Environment</InputLabel>
+                <Select
+                  labelId="ars_url_selector"
+                  value={selected_ars}
+                  label="ARS Environment"
+                  onChange={handleArsSelector}
+                >
+                  {ars_instances.map((ars_instance) => (
+                    <MenuItem
+                      key={ars_instance.url}
+                      value={ars_instance.url}
+                    >
+                      {ars_instance['x-maturity']}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+              {selected_ars && (
+                <>
+                  {!loading_aras ? (
+                    <>
+                      <Autocomplete
+                        className="araSelector"
+                        freeSolo
+                        options={aras}
+                        renderInput={(params) => <TextField {...params} label="ARA" />}
+                        inputValue={ara}
+                        onInputChange={(e, v) => set_ara(v)}
+                      />
+                      <Button
+                        variant="contained"
+                        onClick={get_logs}
+                        disabled={!ara}
+                      >
+                        Load Logs
+                      </Button>
+                      {(logs !== null || loading_logs) && (
+                        <div id="logsContainer">
+                          <h2>Logs:</h2>
+                          {!loading_logs ? (
+                            <Logs logs={logs} />
+                          ) : (
+                            <div className="loading">
+                              <CircularProgress size={100} />
+                            </div>
+                          )}
+                        </div>
+                      )}
+                    </>
                   ) : (
                     <div className="loading">
-                      <CircularProgress size={100} />
+                      <CircularProgress size={300} />
                     </div>
                   )}
-                </div>
+                </>
               )}
             </>
           ) : (
